b1059f656680ce3983710fa4f61b9214084ff4f3
JAMESTHOMSON <JAMESTHOMSON@SHIFTOPSPRO.COM>
2026-03-16 02:26:50 -0400

    chore: finalize rebranding in root (launch scripts and submodule update)

---
+++ launch_shiftops.bat
@@ -0,0 +1,12 @@
+@echo off
+title ShiftOps-OS Launcher
+echo ==========================================
+echo    ShiftOps-OS - Core Intelligence
+echo ==========================================
+echo.
+
+:: Change directory to the ShiftOps-OS\core-intelligence folder
+cd ShiftOps-OS\core-intelligence
+
+:: Call the branded launcher
+call LAUNCH_CORE.bat
